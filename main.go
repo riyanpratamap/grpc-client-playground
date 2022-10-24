31beeac2e6d8913087df3bab2650898d6a6e4cfb
Riyan Pratama <riyan.pratama@outlook.com>
2022-10-24 15:28:11 +0700

    [gRPC] create request in client for calling server

--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"context"
+	"log"
+
+	"github.com/riyanpratamap/grpc-client-playground/chat"
+	"google.golang.org/grpc"
+)
+
 func main() {
+	var conn *grpc.ClientConn
+	conn, err := grpc.Dial(":9000", grpc.WithInsecure())
+	if err != nil {
+		log.Fatal("Erro dial grpc", err)
+	}
+	defer conn.Close()
+
+	c := chat.NewChatServiceClient(conn)
+
+	message := chat.Message{
+		Body: "Hello from client!",
+	}
+	response, err2 := c.SayHello(context.Background(), &message)
+	if err2 != nil {
+		log.Printf("Error when calling SayHello: ", err2)
+	}
+
+	log.Printf("Response from server: %s", response.Body)
 
 }
